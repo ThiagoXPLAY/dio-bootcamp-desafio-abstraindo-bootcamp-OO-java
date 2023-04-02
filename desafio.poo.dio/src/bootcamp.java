653f7fa4a8138d7d6f643fc32e99088cd4cadc33
Thiago <trandrade31@gmail.com>
2023-04-02 12:58:56 -0300

    melhorando a estrutura dos arquivos

--- desafio.poo.dio/src/bootcamp.java
+++
@@ -1,3 +0,0 @@
-public class bootcamp {
-    private String nome;
-}
---
+++ desafio.poo.dio/src/br/com/dio/dominios/bootcamp.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class bootcamp {
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicial = LocalDate.now();
+    private LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<devs> devsInscritos = new HashSet<>();
+    private Set<conteudos> conteudos = new LinkedHashSet<>();
+
+    public bootcamp(String nome, String descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    
+}
